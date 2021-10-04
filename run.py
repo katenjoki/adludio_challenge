41798951df5116ea59c83fa30b0377fd839807f4
katenjoki <katenjoki98@gmail.com>
2021-10-05 00:27:04 +0300

    adding Procfile

--- run.py
+++ run.py
@@ -10,13 +10,10 @@ app = Flask(__name__)
 @app.route('/', methods = ('POST','GET'))
 
 def home():
-    output=None
+    #output=None
     if request.method == 'POST':
         user_input = request.form.get('campaign')
-        out = site_scores(user_input)
-        output = pd.DataFrame(out)
-    else:
-        output = None
+        output = site_scores(user_input)
     
     return render_template('home.html',tables=[output.to_html(classes='data',escape=False)],titles=output.columns.values)
     #return render_template('home.html',outputs=output)
